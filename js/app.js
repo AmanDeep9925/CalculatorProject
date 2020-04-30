208040cbe4e87ca427bae3a37714e90b2d424ea1
AmanDeep9925 <amandeep58257@gmail.com>
2020-04-30 13:26:20 +0530

    Added functionalities to Calcualtor

--- js/app.js
+++ js/app.js
@@ -1,26 +1,57 @@
-var one = document.getElementById("one");
-var plus = document.getElementById("plus");
-var two = document.getElementById("two");
+var buttons = document.getElementsByClassName("table-data");
 var screen = document.getElementById("screen");
-var equal = document.getElementById("equal")
 
-one.addEventListener('click',function(){
-    screen.innerHTML = "1";
-})
+var operand1 = 0;
+var operand2 = null;
+var operator = null;
 
-plus.addEventListener('click',function(){
-    plus.classList.add("bg-color-red");
-    screen.innerHTML += "+";
+function clearScreen(){
+    screen.innerText="";
+}
 
-})
+function add2Screen(value){
+    screen.innerText+=value;
+    operand1 = parseFloat(screen.textContent);
+}
 
-two.addEventListener('click',function(){
-    plus.classList.remove("bg-color-red");
-    screen.innerHTML += "2";
-})
+function arithmeticFunctions(value){
+    screen.innerText+=value;
+}
 
-equal.addEventListener('click',function(){
-    var result = 1 + 2;
-    screen.innerHTML = result;
-});
+function calculate(){
+    var result = eval(screen.innerText);
+    screen.innerText = result;
+}
 
+function changeSign(value){
+    screen.innerText = '-' + screen.innerText;
+}
+
+
+
+for (var i = 0; i < buttons.length; ++i) {
+    buttons[i].addEventListener('click', function () {
+        var value = this.getAttribute('data-value');
+        switch(value){
+            case 'AC' : clearScreen(); break;
+            case '+/-' : changeSign(value); break;
+            case '%' : arithmeticFunctions(value); break;
+            case '/' : arithmeticFunctions(value); break;
+            case '7' : add2Screen(value); break;
+            case '8' : add2Screen(value); break;
+            case '9' : add2Screen(value); break;
+            case '*' : arithmeticFunctions(value); break;
+            case '4' : add2Screen(value); break;
+            case '5' : add2Screen(value); break;
+            case '6' : add2Screen(value); break;
+            case '-' : arithmeticFunctions(value); break;
+            case '1' : add2Screen(value); break;
+            case '2' : add2Screen(value); break;
+            case '3' : add2Screen(value); break;
+            case '+' : arithmeticFunctions(value); break;
+            case '0' : add2Screen(value); break;
+            case '.' : add2Screen(value); break;
+            case '=' : calculate(); break;
+        }
+    });
+}
